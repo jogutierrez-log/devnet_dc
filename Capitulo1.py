585ba13a215320eba44a3405564c9dabcbc7ff60
John <john.gutierrez@la.logicalis.com>
2020-03-04 08:41:46 -0500

    Ejercicios Capitulo 1

---
+++ Capitulo1.py
@@ -0,0 +1,63 @@
+# Ejercicio 1
+print ('EJERCICIO 1')
+print('a. ', 9-3)
+print('b. ',8*2.5)
+print('c.', 9/2)
+print('d. ', 9/-2)
+print('e. ',9//-2)
+print('f. ',9%2)
+print('g. ', 9.0%2)
+print('h. ',9%2.0)
+print('i. ',9%-2)
+print('j. ',-9%2)
+print('k. ',9/-2.0)
+print('l. ',4+3*5)
+print('m. ',(4+3)*5)
+
+# Ejercicio 2
+print("\nEJERCICIO 2")
+x= -17
+print('+x =',+x)
+
+# Ejercicio 3
+print("\nEJERCICIO 3")
+temp= 24
+print('Valor inicial temp (°C): ',temp)
+temp=temp *1.8+32
+print('Valor final temp (°F): ',temp)
+
+# Ejercicio 4
+print("\nEjercicio 4")
+print('a. ', 6 * 3 + 7 * 4)
+print('b. ', 5 + 3 / 4)
+print('c. ', 5 - 2 * 3 ** 4)
+
+# Ejercicio 5
+print("\nEjercicio 5")
+x = 10.5
+print ('a. ', x)
+y =  4
+print ('b. ', y)
+x = x + y
+print('c. x= ',x)
+print('   y= ',y)
+
+# Ejercicio 6
+print("\nEjercicio 6")
+x = 3
+print('Valor inicial de x ',x)
+x += x - x
+print('Valor final de x ',x)
+
+# Ejercicio 7
+print("\nEjercicio 7")
+x = 5
+x= x + z
+
+# Ejercicio 8
+print("\nEjercicio 8")
+print('a. ',6*---------8)
+print('b.  8= people : Error de asignacion')
+print('c. ',((((4**3)))))
+print('d. ',(-(-(-(-5)))))
+print('e. 4+=7/2 : Syntax Error')
